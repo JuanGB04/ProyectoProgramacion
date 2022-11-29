129352929d3f2a18386d4b0573f2695b126f661c
rochh3 <119445362+rochh3@users.noreply.github.com>
2022-11-29 20:28:38 +0100

    Update proyecto.cc

--- proyecto.cc
+++ proyecto.cc
@@ -1,9 +1,45 @@
-#include<iostream>
-using namespace std; 
+#include <iostream>
+
+using namespace std
+//Modo de 4 personas en un mismo tablero sin la IA
+void 4personas (){
+int jug_1, jug_2
+
+
+
+
+
+
+}
+
+
+
 
 int main(){
+    int modo;
 
-cout<<"Esto funciona?"<<endl;
+    //Bienvenida al juego
+    cout<<"Bienvenido al poker panas"<<endl;
+    //Que elija el modo para jugar 
+    cout<<"MODOS:"<<endl<<"4 usuarios en mismo tablero (4)"<<endl<<"1 unico usuario contra la maquina(1)";
+    cout<<"Elige modo al que el usuario quiera jugar"<<endl;
+    cin>>modo;
+    //llamar a las funciones de cada modo de juego
+    cout>>modo;
+
+    //modo de 1 persona contra la IA
+    if(modo == 1){
+    
+
+    }
+
+
+    //modo de 4 personas contra 4
+    if(modo == 4){
+     4personas();   
+
+
+    }
 
 
 return 0;
